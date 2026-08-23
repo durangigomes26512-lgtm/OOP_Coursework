1b29a804f9c7932b9caa2608296670fcf24a6a6e
gitwithaleesha <aleeshanew21@gmail.com>
2026-08-23 20:07:14 +0530

    Refactor Pet class to be abstract and update attributes

--- src/coursework/Pet.java
+++ src/coursework/Pet.java
@@ -1,100 +1,43 @@
 package coursework;
-
-public class Pet {
-
+public abstract class Pet {
     // Attributes
     private String petID;
     private String name;
     private int age;
-    private String gender;
+    private char gender;
     private String healthStatus;
-    private boolean adoptionStatus;
+    private boolean isVaccinated;
+    private boolean isAdopted;
 
     // Parameterized Constructor
-    public Pet(String petID, String name, int age, String gender, String healthStatus) {
-        this.petID = petID;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.healthStatus = healthStatus;
-        this.adoptionStatus = false; // false = Not Adopted
+    public Pet(String petID, String name, int age, char gender, String healthStatus,boolean isVaccinated) {
+		this.petID=petID;
+		this.name=name;
+		this.age=age;
+		this.gender=gender;
+		this.healthStatus=healthStatus;
+		this.isAdopted=false; // false = Not Adopted
+		this.isVaccinated=isVaccinated; 
     }
 
     // Setters
-    public void setPetID(String petID) {
-        this.petID = petID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setHealthStatus(String healthStatus) {
-        this.healthStatus = healthStatus;
-    }
-
-    public void setAdoptionStatus(boolean adoptionStatus) {
-        this.adoptionStatus = adoptionStatus;
-    }
-
+	//no setters for petID, name and gender bcz they usually stay the same
+	public void setAge(int age) {this.age=age;}
+	public void setHealthStatus(String health) {this.healthStatus=health;}
+	public void setIsAdopted(boolean isadopted) {this.isAdopted=isadopted;}
+	public void setIsVaccinated(boolean isvaccinated) {this.isVaccinated=isvaccinated;}
     // Getters
-    public String getPetID() {
-        return petID;
-    }
+    public String getPetID() {return petID;}
+    public String getName() {return name;}
+    public int getAge() {return age;}
+    public char getGender() {return gender;}
+    public String getHealthStatus() {return healthStatus;}
+    public boolean getAdoptionStatus() {return isAdopted;}
+    public boolean isVaccinated() {return isVaccinated;}
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getHealthStatus() {
-        return healthStatus;
-    }
-
-    public boolean getAdoptionStatus() {
-        return adoptionStatus;
-    }
-
+    //below classes must be overridden by dog and cat class
     // Method to calculate adoption fee
-    public double calculateAdoptionFee() {
-        return 0.0;
-    }
-
+    public abstract double calculateAdoptionFee();
     // General care instructions
-    public String getCareInstructions() {
-    	return "Please give the pet food, clean water and take good care of it";
-    }
-
-    // Display pet information
-    public void displayPetInfo() {
-        System.out.println("Pet ID: " + petID);
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Gender: " + gender);
-        System.out.println("Health Status: " + healthStatus);
-
-        if (adoptionStatus) {
-            System.out.println("Adoption Status: Adopted");
-        } else {
-            System.out.println("Adoption Status: Not Adopted");
-        }
-    }
+	public abstract String getCareInstructions();
 }
-   
-    	
-    
