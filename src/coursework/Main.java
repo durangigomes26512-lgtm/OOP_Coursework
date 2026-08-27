1d979ffceda976028909a799311f84b40af006a0
Mubashira Akram <mubaa365@gmail.com>
2026-08-27 20:19:14 +0530

    Update Main.java

--- src/coursework/Main.java
+++ src/coursework/Main.java
@@ -14,56 +14,7 @@ public class Main {
         ShelterManager manager =
                 new ShelterManager();
 
-        // Sample pets
-
-        manager.addPet(
-                new Dog(
-                        1,
-                        "Max",
-                        3,
-                        "Male",
-                        "Healthy",
-                        "Labrador",
-                        true
-                )
-        );
-
-        manager.addPet(
-                new Dog(
-                        2,
-                        "Charlie",
-                        4,
-                        "Male",
-                        "Healthy",
-                        "Golden Retriever",
-                        true
-                )
-        );
-
-        manager.addPet(
-                new Cat(
-                        3,
-                        "Luna",
-                        2,
-                        "Female",
-                        "Healthy",
-                        true,
-                        true
-                )
-        );
-
-        manager.addPet(
-                new Cat(
-                        4,
-                        "Milo",
-                        1,
-                        "Male",
-                        "Healthy",
-                        true,
-                        true
-                )
-        );
-
+     
         SwingUtilities.invokeLater(() -> {
 
             PetAdoptionGUI gui =
